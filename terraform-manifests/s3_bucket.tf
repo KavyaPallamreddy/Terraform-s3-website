6eb61ce2c5a6e577a8cbfe8c7d5b27707ccbb4f2
Kavya <116321339+KavyaPallamreddy@users.noreply.github.com>
2023-01-13 17:24:50 -0700

    Update s3_bucket.tf

--- terraform-manifests/s3_bucket.tf
+++ terraform-manifests/s3_bucket.tf
@@ -1,7 +1,8 @@
 # Call our Custom Terraform Module which we built earlier
 
 module "website_s3_bucket" {
-  source = "./modules/aws-s3-static-website-bucket"  # Mandatory
+  source  = "app.terraform.io/kavyareddy/website/s3"
+  version = "1.0.1"
   bucket_name = var.my_s3_bucket
   tags = var.my_s3_tags
 }
